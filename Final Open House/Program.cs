e27fd2038b12ab6e3a6041cf6eafd3355d92e50a
Daniel Gonzalez <dgonzalez7752@gmail.com>
2019-01-29 16:45:43 -0600

    Add project files.

---
+++ Final Open House/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Open_House
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World!");
+            }
+        }
+    }
